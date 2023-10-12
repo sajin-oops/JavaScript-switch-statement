18dd8f8f3e75a96d7137c734778f0ceb300b7e09
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-12 18:57:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let food = "grill"
+switch(food){
+  case 'grill':
+    console.log("grill chicken is great");
+    break;
+  case 'chicken':
+    console.log("chicken is great");
+    break;
+  case 'biriyani':
+    console.log("biriyani is great");
+    break;
+  case 'friedrice':
+    console.log("fried rice is great");
+    break
+  default:
+    console.log("Not in the menu");
+}
